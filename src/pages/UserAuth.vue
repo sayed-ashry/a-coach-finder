846186aab55630b509dd7d802c69dc53e621e0cb
sayed <sayedashry1@gmail.com>
2024-03-10 14:00:01 +0200

    add navigation guard

--- src/pages/UserAuth.vue
+++ src/pages/UserAuth.vue
@@ -74,7 +74,8 @@ export default {
         } else {
           await this.$store.dispatch("signup", user);
         }
-        this.$router.replace('/coaches')
+        const redirectUrl = "/" + (this.$route.query.redirect || "coaches");
+        this.$router.replace(redirectUrl);
       } catch (err) {
         this.error = err.message || "Failed to authenticate.try later";
       }
--- src/router.js
+++ src/router.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from "vue-router";
 import CoachesList from "./pages/CoachesListPage.vue";
+import store from "./store";
 
 const router = createRouter({
   history: createWebHistory(),
@@ -23,18 +24,39 @@ const router = createRouter({
     {
       path: "/register",
       component: () => import("./pages/CoachRegistrationPage.vue"),
+      meta: {
+        requireAuth: true,
+      },
     },
+
     {
       path: "/requests",
       component: () => import("./pages/RequestsReceivedPage.vue"),
+      meta: {
+        requireAuth: true,
+      },
     },
     {
       path: "/:notFound(.*)",
       component: () => import("./pages/NotFoundPage.vue"),
     },
 
-    { path: "/auth", component: () => import("./pages/UserAuth.vue") },
+    {
+      path: "/auth",
+      component: () => import("./pages/UserAuth.vue"),
+      meta: {
+        requireUnAuth: true,
+      },
+    },
   ],
 });
-
+router.beforeEach((to, _, next) => {
+  if (to.meta.requireAuth && !store.getters.isAuthenticated) {
+    next("/auth");
+  } else if (to.meta.requireUnAuth && store.getters.isAuthenticated) {
+    next("/coaches");
+  } else {
+    next();
+  }
+});
 export default router;
